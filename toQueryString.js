2ea661752697a2122004f2052b4f98f3e221bb8c
YuliiaVovk <dp100193vjj@gmail.com>
2017-09-26 18:08:04 +0300

    Add files via upload

--- toQueryString.js
+++ toQueryString.js
@@ -1,13 +1,13 @@
 // "?first=1&second=2&third=3"
 function toQueryString(obj) {
-    var str = "";
-    var first = true;
-    for (var i in obj){
-    	if (first) str += "?";
-        if (!first) str += "&";
-        str += i + "=" + obj[i];
-        first = false;
-    }
-    return str;
+	var str = "";
+	var first = true;
+	for (var i in obj){
+		if (first) str += "?";
+		if (!first) str += "&";
+		str += i + "=" + obj[i];
+		first = false;
+	}
+	return str;
 }
-toQueryString({first: 1, second: 2, third: 3}); 
+toQueryString({first: 1, second: 2, third: 3});
