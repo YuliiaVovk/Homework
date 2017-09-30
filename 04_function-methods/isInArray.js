52694adb7012d8e79fbd050758e4a538d46b6443
YuliiaVovk <dp100193vjj@gmail.com>
2017-09-30 19:00:50 +0300

    Add files via upload

--- 04_function-methods/isInArray.js
+++ 04_function-methods/isInArray.js
@@ -1,20 +1,28 @@
-if (!Array.isArray) {
-	Array.isArray = function(value,arrItem) {
-		return Object.prototype.toString.call(arg) === arrItem;
-	};
-}
-
 var a = {};
 var totalArr = [{}, a, [], 10];
 function isInArray(item,array) {
 	"use strict";
 	return array.indexOf(item) !== -1;}
 isInArray(a, totalArr);
-
+//true 
 
 function splash (itemCheck,arrLast){
 if (arrLast.indexOf(itemCheck) !== -1) {
-    return true;
+	return true;
 }
 };
-splash ('3', [1, 2, 3, 4] )
+splash ('3', [1, 2, 3, 4] );
+//undefined
+
+function splash (str,[str2]) {
+	'use strict';
+	for (var i = 0; i < str2.length; i++) {
+		if(str.indexOf(str2[i]) == -1) {
+		return false;
+	}
+}
+	return true;
+}
+
+splash('3', [1, 2, 3, 4] );
+//undefined
